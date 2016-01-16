c4d0d47ab6923d57553101ca7588d4997b37c6ad
Pavel Georgiev <pavel@datagridsys.com>
2016-01-16 12:52:05 -1000

    Rename ansible group to datagrid

--- dgri_client_install.sh
+++ dgri_client_install.sh
@@ -15,7 +15,7 @@ sudo rpm -Uvh https://s3.amazonaws.com/dgri-rpm-repo/x86_64/dgri-rpm-repo-1-0.1.
 sudo yum install -y ansible-dgri-modules dgri-query >/dev/null
 
 # Create a sample ansible group (only localhost) that we will collect data from
-echo -e '\n[my-ansible-group]\nlocalhost\n' |sudo tee -a /etc/ansible/hosts > /dev/null
+echo -e '\n[datagrid]\nlocalhost\n' |sudo tee -a /etc/ansible/hosts > /dev/null
 
 # Done, print configuration instructions
 cat << EOF
@@ -28,12 +28,12 @@ To configure your systems:
 
 1) Set your username and password in /etc/ansible-dgri-modules.conf
 
-2) (Optional) Add more hosts to the ansible group [my-ansible-group] in
+2) (Optional) Add more hosts to the ansible group [datagrid] in
    /etc/ansible/hosts. Remove localhost if not needed
 
 3) Make sure ansible (running as root) can ssh to the hosts in
-   group [my-ansible-group]. To verify:
-# sudo ansible -m ping my-ansible-group
+   group [datagrid]. To verify:
+# sudo ansible -m ping datagrid
 
 To collect data from servers (will be done by a cronjob):
 # sudo ansible-playbook /usr/share/dgri/ansible-playbooks/dgri.yml
